361268e0b8616b5747fe2ca5d310b8d1aa9fd474
Jin Kwon <onacit@gmail.com>
2016-04-26 19:24:00 +0900

    updated

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,35 +1,21 @@
 "use-strict";
 
+var logger = log4javascript.getLogger("index.js");
+logger.addAppender(new log4javascript.BrowserConsoleAppender());
+//logger.log("level: " + logger.getLevel());
+logger.setLevel(log4javascript.Level.DEBUG);
+
 var config;
 
 $(document).ready(function () {
-    console.log("$(document).ready()");
+    //console.log("$(document).ready()");
+    logger.debug("$(document).ready()");
     $.getJSON({
         url: 'configs/default.json',
         success: function (data) {
             config = data;
             console.log('baseurl: ' + config.baseurl);
         }});
-    /*
-     $.ajax({
-     url: 'config/default.json',
-     beforeSend: function (xhr) {
-     if (xhr.overrideMimeType) {
-     xhr.overrideMimeType("application/json");
-     }
-     },
-     dataType: "json",
-     success: function (data, status, jqxhr) {
-     console.log('data: ' + data);
-     console.log('status: ' + status);
-     console.log('jqxhr: ' + jqxhr);
-     config = data;
-     console.log('baseurl: ' + config.baseurl);
-     },
-     error: function (jqxhr) {
-     }
-     });
-     */
 });
 
 $(window).on("load", function () {
---
+++ src/scripts/index2.js
@@ -0,0 +1,9 @@
+"use-strict";
+
+//var logger = log4javascript.getLogger("index.js");
+//logger.addAppender(new log4javascript.BrowserConsoleAppender());
+
+$(document).ready(function () {
+    logger.debug("index2: $(document).ready()");
+});
+
